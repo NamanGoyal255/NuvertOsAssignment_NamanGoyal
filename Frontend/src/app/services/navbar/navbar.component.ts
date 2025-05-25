bbdd318caaa60064f4f5a2203079dc95d6cbccb2
NamanGoyal255 <naman@datadrew.io>
2025-05-25 21:18:18 +0530

    FrontendComplete

--- Frontend/src/app/services/navbar/navbar.component.ts
+++ Frontend/src/app/services/navbar/navbar.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../auth.service';
 
 @Component({
   selector: 'navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css']
 })
-export class NavbarComponent {
+export class NavbarComponent implements OnInit {
+  loggedIn = false;
+  hasCookie=false;
+  constructor(private authService: AuthService) {}
+
+  ngOnInit(): void {
+    
+    if(this.authService.getToken()){
+      this.hasCookie=true;
+    }
+    else{
+      this.hasCookie=false;
+    }
+    this.loggedIn = this.hasCookie;
+  }
+
+  logout(): void {
+    this.authService.logout();
+    this.loggedIn = false;
+  }
 
 }
